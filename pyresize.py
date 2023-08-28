07ad786eb05f7c69f7d3d03d16debee4d31165d8
Alex Dillhoff <ajdillhoff@gmail.com>
2023-08-28 15:19:11 -0500

    Updated with our new feature.

--- pyresize.py
+++ pyresize.py
@@ -1,12 +1,12 @@
 import sys
 from PIL import Image
 
-def resize_image(image_path, width):
+def resize_image(image_path, width, output_path):
     image = Image.open(image_path)
     wpercent = (width / float(image.size[0]))
     hsize = int((float(image.size[1]) * float(wpercent)))
     image = image.resize((width, hsize), Image.LANCZOS)
-    image.save(image_path)
+    image.save(output_path)
 
 if __name__ == "__main__":
-    resize_image(sys.argv[1], int(sys.argv[2]))
+    resize_image(sys.argv[1], int(sys.argv[2]), sys.argv[3])
